95e207a3ecaf627363369c10ecaca41cdfaf7eea
CHE TRAN <chetran@CHEs-MacBook-Pro.local>
2024-11-27 14:29:09 +0100

    writing to firebase

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from "express";
+import { initializeApp, cert } from "firebase-admin/app";
+import { getDatabase } from "firebase-admin/database";
+import { readFile } from "fs/promises";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const serviceAccount = JSON.parse(
+  await readFile(new URL("./tentamina.json", import.meta.url), "utf-8")
+);
+
+initializeApp({
+  credential: cert(serviceAccount),
+  databaseURL: process.env.DATABASE_URL,
+});
+
+const db = getDatabase();
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
@@ -22,9 +39,16 @@ app.post("/login", (req, res) => {
 app.get("/new", (req, res) => {
   res.render("new.ejs");
 });
-app.post("/new", (req, res) => {
+app.post("/new", async (req, res) => {
   console.log(req.body);
-  res.render("admin.ejs");
+  let message = "Added Tenta";
+  try {
+    const newRef = await db.ref("tentor").push(req.body);
+  } catch (error) {
+    message = error.message;
+    res.status(500);
+  }
+  res.render("admin.ejs", { message: message });
 });
 app.get("/showall", (req, res) => {
   res.send("showall");
--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
-Make sure to install nodemon
-To run (command line):
+## Make sure to install nodemon.
+
+## To run portal:
 
 - npm init
+- create .env and add "DATABASE_URL=<Your database url, can be found in project->realtime database, you might need to create one>
+- you need to generate key, in firebase project settings->service accounts->generate key
+- add generated json file and rename to: tentamina.json
 - nodemon index.js
