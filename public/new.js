c5e0fea24d5f181f242330e5fa58b16924eeb45f
chetran <chelongtran@gmail.com>
2024-12-18 16:03:31 +0100

    Bug. [backend] fixed question number

--- public/new.js
+++ public/new.js
@@ -1,17 +1,17 @@
 document.querySelector("#newQ").addEventListener("click", () => {
-  const questions = document.querySelectorAll(".question");
+  const questions = document.querySelectorAll(".questions");
   const input = document.createElement("input");
   input.setAttribute("name", `questions`);
   input.setAttribute("placeholder", `Question ${questions.length + 1}`);
   input.setAttribute("required", "");
   const li = document.createElement("li");
-  li.setAttribute("class", "question");
+  li.setAttribute("class", "questions");
   li.append(input);
-  document.querySelector(".questions").append(li);
+  document.querySelector(".questions-list").append(li);
   console.log(questions);
 });
 
 document.querySelector("#removeQ").addEventListener("click", () => {
-  const questions = document.querySelectorAll(".question");
+  const questions = document.querySelectorAll(".questions");
   questions[questions.length - 1].remove();
 });
